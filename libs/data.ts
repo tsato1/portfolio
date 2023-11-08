2cc5bd10ec766f0c8dec08dd69bcd69511c7f558
tsato1 <a8tb1117@gmail.com>
2023-11-08 11:47:54 +0900

    image files reorganized

--- libs/data.ts
+++ libs/data.ts
@@ -1,39 +1,39 @@
 import { ExperienceData, TechStack, WorkData } from "./types"
 
 export const skills = [
-  { id: 100, name: 'Next JS', imageUrl: '/next.svg', category: ['web'] } as TechStack,
-  { id: 101, name: 'React', imageUrl: '/react.png', category: ['web'] } as TechStack,
-  { id: 102, name: 'Typescript', imageUrl: '/typescript.png', category: ['web'] } as TechStack,
-  { id: 103, name: 'Tailwindcss', imageUrl: '/tailwind.png', category: ['web'] } as TechStack,
-  { id: 104, name: 'Framer Motion', imageUrl: '/framermotion.png', category: ['web'] } as TechStack,
-  // { id: 105, name: 'Node JS', imageUrl: '/nodejs.png', category: ['web'] } as TechStack,
+  { id: 100, name: 'Next JS', imageUrl: '/icons/techstack/frontend/next.svg', category: ['web'] } as TechStack,
+  { id: 101, name: 'React', imageUrl: '/icons/techstack/frontend/react.png', category: ['web'] } as TechStack,
+  { id: 102, name: 'Typescript', imageUrl: '/icons/techstack/frontend/typescript.png', category: ['web'] } as TechStack,
+  { id: 103, name: 'Tailwindcss', imageUrl: '/icons/techstack/frontend/tailwind.png', category: ['web'] } as TechStack,
+  { id: 104, name: 'Framer Motion', imageUrl: '/icons/techstack/frontend/framermotion.png', category: ['web'] } as TechStack,
+  // { id: 105, name: 'Node JS', imageUrl: '/icons/techstack/frontend/nodejs.png', category: ['web'] } as TechStack,
 
-  { id: 200, name: 'Kotlin', imageUrl: '/kotlin.png', category: ['mobile'] } as TechStack,
-  { id: 201, name: 'Ktor', imageUrl: '/ktor.png', category: ['mobile'] } as TechStack,
-  { id: 202, name: 'Gradle', imageUrl: '/gradle.png', category: ['mobile'] } as TechStack,
-  // { id: 203, name: 'Java', imageUrl: '/java.png', category: ['mobile']} as TechStack,
-  // { id: 204, name: 'SpringBoot', imageUrl: '/springboot.png', category: ['mobile']} as TechStack,
+  { id: 200, name: 'Kotlin', imageUrl: '/icons/techstack/mobile/kotlin.png', category: ['mobile'] } as TechStack,
+  { id: 201, name: 'Ktor', imageUrl: '/icons/techstack/backend/ktor.png', category: ['mobile'] } as TechStack,
+  { id: 202, name: 'Gradle', imageUrl: '/icons/techstack/mobile/gradle.png', category: ['mobile'] } as TechStack,
+  // { id: 203, name: 'Java', imageUrl: '/icons/techstack/backend/java.png', category: ['mobile']} as TechStack,
+  // { id: 204, name: 'SpringBoot', imageUrl: '/icons/techstack/backend/springboot.png', category: ['mobile']} as TechStack,
 
-  { id: 300, name: 'MySQL', imageUrl: '/mysql.png', category: ['db', 'web', 'mobile'] } as TechStack,
-  { id: 301, name: 'SQLite', imageUrl: '/sqlite.png', category: ['db', 'mobile'] } as TechStack,
-  { id: 302, name: 'Mongo DB', imageUrl: '/mongodb.png', category: ['db', 'web', 'mobile'] } as TechStack,
-  // { id: 303, name: 'Postgresql', imageUrl: '/postgresql.png', category: ['db', 'web', 'mobile']} as TechStack,
-  // { id: 304, name: 'Graphql', imageUrl: '/graphql.png', category: ['db', 'web'] } as TechStack,
-  { id: 305, name: 'Planetscale', imageUrl: '/planetscale.png', category: ['db', 'web'] } as TechStack,
-  { id: 306, name: 'Prisma', imageUrl: '/prisma.png', category: ['db', 'web'] } as TechStack,
+  { id: 300, name: 'MySQL', imageUrl: '/icons/techstack/db/mysql.png', category: ['db', 'web', 'mobile'] } as TechStack,
+  { id: 301, name: 'SQLite', imageUrl: '/icons/techstack/db/sqlite.png', category: ['db', 'mobile'] } as TechStack,
+  { id: 302, name: 'Mongo DB', imageUrl: '/icons/techstack/db/mongodb.png', category: ['db', 'web', 'mobile'] } as TechStack,
+  // { id: 303, name: 'Postgresql', imageUrl: '/icons/techstack/db/postgresql.png', category: ['db', 'web', 'mobile']} as TechStack,
+  // { id: 304, name: 'Graphql', imageUrl: '/icons/techstack/db/graphql.png', category: ['db', 'web'] } as TechStack,
+  { id: 305, name: 'Planetscale', imageUrl: '/icons/techstack/db/planetscale.png', category: ['db', 'web'] } as TechStack,
+  { id: 306, name: 'Prisma', imageUrl: '/icons/techstack/db/prisma.png', category: ['db', 'web'] } as TechStack,
 
-  // { id: 400, name: 'AWS', imageUrl: '/aws.png', category: ['infra'] } as TechStack,
-  { id: 401, name: 'SST', imageUrl: '/sst.png', category: ['infra'] } as TechStack,
-  { id: 402, name: 'Docker', imageUrl: '/docker.png', category: ['infra'] } as TechStack,
-  { id: 403, name: 'Vercel', imageUrl: '/vercel.png', category: ['infra'] } as TechStack,
-  // { id: 404, name: 'Postman', imageUrl: '/postman.png', category: ['infra'] } as TechStack,
-  // { id: 502, name: 'Mailgun', imageUrl: '/mailgun.png', category: ['other']} as TechStack
+  // { id: 400, name: 'AWS', imageUrl: '/icons/techstack/aws.png', category: ['infra'] } as TechStack,
+  { id: 401, name: 'SST', imageUrl: '/icons/techstack/sst.png', category: ['infra'] } as TechStack,
+  { id: 402, name: 'Docker', imageUrl: '/icons/techstack/docker.png', category: ['infra'] } as TechStack,
+  { id: 403, name: 'Vercel', imageUrl: '/icons/techstack/frontend/vercel.png', category: ['infra'] } as TechStack,
+  // { id: 404, name: 'Postman', imageUrl: '/icons/techstack/postman.png', category: ['infra'] } as TechStack,
+  // { id: 502, name: 'Mailgun', imageUrl: '/icons/techstack/mailgun.png', category: ['other']} as TechStack
 ] as const
 
 export const experiences = [
   {
     companyName: 'Interestix',
-    companyLogoUrl: '/interestix.png',
+    companyLogoUrl: '/icons/companyLogos/interestix.png',
     title: 'Android Developer / Tester',
     workingPeriod: '2016.1-2016.8',
     jobDescriptions: [
@@ -42,24 +42,24 @@ export const experiences = [
       'Wrote JUnit code for conducting comprehensive unit tests.',
     ],
     skills: [
-      { imageUrl: '/java.png', name: 'Java' },
-      { imageUrl: '/sqlite.png', name: 'SQLite' },
-      { imageUrl: '/junit.png', name: 'Junit4' },
+      { imageUrl: '/icons/techstack/backend/java.png', name: 'Java' },
+      { imageUrl: '/icons/techstack/db/sqlite.png', name: 'SQLite' },
+      { imageUrl: '/icons/techstack/mobile/junit.png', name: 'Junit4' },
       { imageUrl: '', name: 'Working In Team' }
     ],
     testimonials: [
       {
-        imageUrl: '/farnyu.jpeg',
-        name: 'Farn-Yu Khong',
+        imageUrl: '',
+        name: 'F.K.',
         position: 'CTO',
         words: 'He excels in both expressing his opinions and actively listening to others. His strong communication skills contribute significantly to effective collaboration and teamwork.',
-        linkedInUrl: 'https://www.linkedin.com/in/farn-yu-khong-2607b628/'
+        linkedInUrl: ''
       }
     ]
   } as ExperienceData,
   {
     companyName: 'ZL Technologies',
-    companyLogoUrl: '/zltech.png',
+    companyLogoUrl: '/icons/companyLogos/zltech.png',
     title: 'Product Analyst',
     workingPeriod: '2016.8-2019.7',
     jobDescriptions: [
@@ -69,9 +69,9 @@ export const experiences = [
       'Provided dedicated client support, delivering comprehensive explanations regarding the health of their system.'
     ],
     skills: [
-      { imageUrl: '/html.png', name: 'HTML' },
-      { imageUrl: '/css.png', name: 'CSS' },
-      { imageUrl: '/javascript.png', name: 'Javascript' },
+      { imageUrl: '/icons/techstack/frontend/html.png', name: 'HTML' },
+      { imageUrl: '/icons/techstack/frontend/css.png', name: 'CSS' },
+      { imageUrl: '/icons/techstack/frontend/javascript.png', name: 'Javascript' },
       { imageUrl: '', name: 'Project Management' },
       { imageUrl: '', name: 'Working with Teams' },
       { imageUrl: '', name: 'Consulting on Tech' },
@@ -79,17 +79,17 @@ export const experiences = [
     ],
     testimonials: [
       {
-        imageUrl: '/niki.jpeg',
-        name: 'Niharika Bhardwaj',
+        imageUrl: '',
+        name: 'N.B.',
         position: 'Product Analyst',
         words: 'Tak possesses a clear focus. While he demonstrates a great attention to detail, he also balance this meticulous approach with efficiency for swift progress and adaptation.',
-        linkedInUrl: 'https://www.linkedin.com/in/niharikabhardwaj15/'
+        linkedInUrl: ''
       }
     ]
   } as ExperienceData,
   {
     companyName: 'Fun Podium',
-    companyLogoUrl: '/funpodium.png',
+    companyLogoUrl: '/icons/companyLogos/funpodium.png',
     title: 'Android Developer',
     workingPeriod: '2022.9-2022.12',
     jobDescriptions: [
@@ -98,9 +98,9 @@ export const experiences = [
       'Maintained close communication with the design team to facilitate progress and address issues collaboratively.'
     ],
     skills: [
-      { imageUrl: '/android.png', name: 'Android' },
-      { imageUrl: '/kotlin.png', name: 'Kotlin' },
-      { imageUrl: '/jetpack.png', name: 'Jetpack Library' },
+      { imageUrl: '/icons/techstack/mobile/android.png', name: 'Android' },
+      { imageUrl: '/icons/techstack/mobile/kotlin.png', name: 'Kotlin' },
+      { imageUrl: '/icons/techstack/mobile/jetpack.png', name: 'Jetpack Library' },
       { imageUrl: '', name: 'Corutine' },
       { imageUrl: '', name: 'MVVM' },
       { imageUrl: '', name: 'Clean Architecture' },
@@ -126,13 +126,13 @@ export const experiences = [
       'Teaching Java with focus on Algorithm and Data Structure.'
     ],
     skills: [
-      { imageUrl: '/nextjs.png', name: 'Next JS' },
-      { imageUrl: '/react.png', name: 'React' },
-      { imageUrl: "/typescript.png", name: "Typescript" },
-      { imageUrl: "/tailwind.png", name: "Tailwind CSS" },
-      { imageUrl: '/android.png', name: 'Android' },
-      { imageUrl: '/kotlin.png', name: 'Kotlin' },
-      { imageUrl: '/java.png', name: 'Java' },
+      { imageUrl: '/icons/techstack/frontend/nextjs.png', name: 'Next JS' },
+      { imageUrl: '/icons/techstack/frontend/react.png', name: 'React' },
+      { imageUrl: "/icons/techstack/frontend/typescript.png", name: "Typescript" },
+      { imageUrl: "/icons/techstack/frontend/tailwind.png", name: "Tailwind CSS" },
+      { imageUrl: '/icons/techstack/mobile/android.png', name: 'Android' },
+      { imageUrl: '/icons/techstack/mobile/kotlin.png', name: 'Kotlin' },
+      { imageUrl: '/icons/techstack/backend/java.png', name: 'Java' },
       { imageUrl: '', name: 'Algorithm' },
       { imageUrl: '', name: 'Data Structure' },
       { imageUrl: '', name: 'Customer Success' },
@@ -148,21 +148,21 @@ export const works = [
     description: 'Management console designed for organizaing multiple online stores with ease',
     category: 'web',
     imageUrls: [
-      '/ec-admin1.png',
-      '/ec-admin2.png',
-      '/ec-admin3.png',
+      '/works/ec-admin1.png',
+      '/works/ec-admin2.png',
+      '/works/ec-admin3.png',
     ],
     isLive: true,
     skills: [
-      { imageUrl: "/nextjs.png", name: "Next Js" },
-      { imageUrl: "/react.png", name: "React" },
-      { imageUrl: "/typescript.png", name: "Typescript" },
-      { imageUrl: "/tailwind.png", name: "Tailwind CSS" },
-      { imageUrl: "/shadcnui.png", name: "Shadcn/ui" },
-      { imageUrl: "/planetscale.png", name: "Planetscale" },
-      { imageUrl: "/prisma.png", name: "Prisma" },
-      { imageUrl: "/mysql.png", name: "MySQL" },
-      { imageUrl: "/vercel.png", name: "Vercel" },
+      { imageUrl: "/icons/techstack/frontend/nextjs.png", name: "Next Js" },
+      { imageUrl: "/icons/techstack/frontend/react.png", name: "React" },
+      { imageUrl: "/icons/techstack/frontend/typescript.png", name: "Typescript" },
+      { imageUrl: "/icons/techstack/frontend/tailwind.png", name: "Tailwind CSS" },
+      { imageUrl: "/icons/techstack/frontend/shadcnui.png", name: "Shadcn/ui" },
+      { imageUrl: "/icons/techstack/db/planetscale.png", name: "Planetscale" },
+      { imageUrl: "/icons/techstack/db/prisma.png", name: "Prisma" },
+      { imageUrl: "/icons/techstack/db/mysql.png", name: "MySQL" },
+      { imageUrl: "/icons/techstack/frontend/vercel.png", name: "Vercel" },
     ],
     link: ''
   } as WorkData,
@@ -171,17 +171,17 @@ export const works = [
     description: 'Ecommerce store that works in conjunction with ECommerce Admin Suite',
     category: 'web',
     imageUrls: [
-      '/ec-store1.png',
-      '/ec-store2.png',
-      '/ec-store3.png',
+      '/works/ec-store1.png',
+      '/works/ec-store2.png',
+      '/works/ec-store3.png',
     ],
     isLive: true,
     skills: [
-      { imageUrl: "/nextjs.png", name: "Next Js" },
-      { imageUrl: "/react.png", name: "React" },
-      { imageUrl: "/typescript.png", name: "Typescript" },
-      { imageUrl: "/tailwind.png", name: "Tailwind CSS" },
-      { imageUrl: "/vercel.png", name: "Vercel" },
+      { imageUrl: "/icons/techstack/frontend/nextjs.png", name: "Next Js" },
+      { imageUrl: "/icons/techstack/frontend/react.png", name: "React" },
+      { imageUrl: "/icons/techstack/frontend/typescript.png", name: "Typescript" },
+      { imageUrl: "/icons/techstack/frontend/tailwind.png", name: "Tailwind CSS" },
+      { imageUrl: "/icons/techstack/frontend/vercel.png", name: "Vercel" },
     ],
     link: ''
   } as WorkData,
@@ -190,20 +190,20 @@ export const works = [
     description: 'Simple Android app that keeps track of your income and spendings',
     category: 'mobile',
     imageUrls: [
-      '/kakeibo1.png',
-      '/kakeibo2.png',
-      '/kakeibo3.png',
-      '/kakeibo4.png',
-      '/kakeibo5.png',
-      '/kakeibo6.png',
-      '/kakeibo7.png',
+      '/works/kakeibo1.png',
+      '/works/kakeibo2.png',
+      '/works/kakeibo3.png',
+      '/works/kakeibo4.png',
+      '/works/kakeibo5.png',
+      '/works/kakeibo6.png',
+      '/works/kakeibo7.png',
     ],
     isLive: true,
     skills: [
-      { imageUrl: "/android.png", name: "Android" },
-      { imageUrl: "/kotlin.png", name: "Kotlin" },
-      { imageUrl: '/jetpack.png', name: 'Jetpack Library' },
-      { imageUrl: "/sqlite.png", name: "SQLite" },
+      { imageUrl: "/icons/techstack/mobile/android.png", name: "Android" },
+      { imageUrl: "/icons/techstack/mobile/kotlin.png", name: "Kotlin" },
+      { imageUrl: '/icons/techstack/mobile/jetpack.png', name: 'Jetpack Library' },
+      { imageUrl: "/icons/techstack/db/sqlite.png", name: "SQLite" },
       { imageUrl: "", name: "MVVM" },
       { imageUrl: "", name: "Clean Architecture" },
       { imageUrl: "", name: "Dagger Hilt" },
@@ -215,16 +215,16 @@ export const works = [
     name: 'This portfolio website',
     description: 'Simple web app with animation',
     category: 'web',
-    imageUrls: ['/portfolio.png'],
+    imageUrls: ['/works/portfolio.png'],
     isLive: true,
     skills: [
-      { imageUrl: "/nextjs.png", name: "Next Js" },
-      { imageUrl: "/react.png", name: "React" },
-      { imageUrl: "/typescript.png", name: "Typescript" },
-      { imageUrl: "/tailwind.png", name: "Tailwind CSS" },
-      { imageUrl: "/framermotion.png", name: "Framer Motion" },
-      { imageUrl: "/sst.png", name: "SST" },
-      { imageUrl: "/aws-ses.png", name: "AWS SES" },
+      { imageUrl: "/icons/techstack/frontend/nextjs.png", name: "Next Js" },
+      { imageUrl: "/icons/techstack/frontend/react.png", name: "React" },
+      { imageUrl: "/icons/techstack/frontend/typescript.png", name: "Typescript" },
+      { imageUrl: "/icons/techstack/frontend/tailwind.png", name: "Tailwind CSS" },
+      { imageUrl: "/icons/techstack/frontend/framermotion.png", name: "Framer Motion" },
+      { imageUrl: "/icons/techstack/sst.png", name: "SST" },
+      { imageUrl: "/icons/techstack/aws-ses.png", name: "AWS SES" },
     ],
     link: ''
   } as WorkData,
