c8296bd53ac2faa31ff5011ee4834d5733e82dc9
tsato1 <9661884+tsato1@users.noreply.github.com>
2024-07-29 12:12:04 +0800

    feat: twitter icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
+import { BsLinkedin, BsTwitter } from 'react-icons/bs'
 
 const Footer = () => {
   return (
     <footer className='mb-10 px-4  text-center text-gray-500'>
+      <div className='flex items-center justify-center'>
+        <Link href='https://x.com/takahide_sato' target="_blank">
+          <BsTwitter className='w-5 h-5' />
+        </Link>
+      </div>
       <small className='mb-2 text-xs'>
         &copy; 2023. all rights reserved
       </small>
